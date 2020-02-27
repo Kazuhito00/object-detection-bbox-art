e26120f5a84e834f840bbe33e961f023580203ad
takahashi <shigeki.takahashi.dev@gmail.com>
2020-02-27 23:20:56 +0900

    注釈線にて、画面右端からはみ出した際、反転するように修正

--- boundingbox_art/bba_annotation_line.py
+++ boundingbox_art/bba_annotation_line.py
@@ -29,24 +29,29 @@ def bba_annotation_line(
 
     font_size = int(rectangle_width * (1 / 10))
 
+    drawpoint1 = (int((x1 + x2) / 2), int((y1 + y2) / 2))
+    drawpoint2 = (x2 - int(rectangle_width / 10),
+                  y1 + int(rectangle_height / 10))
+    drawpoint3 = (x2 + int(rectangle_width / 2),
+                  y1 + int(rectangle_height / 10))
+    textpoint = (drawpoint2[0], drawpoint2[1] - int(font_size * 1.5))
+
+    if drawpoint3[0] > image.shape[1]:
+        drawpoint2 = (x1 + int(rectangle_width / 10),
+                      y1 + int(rectangle_height / 10))
+        drawpoint3 = (x1 - int(rectangle_width / 2),
+                      y1 + int(rectangle_height / 10))
+        textpoint = (drawpoint3[0], drawpoint2[1] - int(font_size * 1.5))
+
     cv.circle(
-        draw_image, (int((x1 + x2) / 2), int((y1 + y2) / 2)),
-        int(rectangle_width / 40),
-        color,
-        thickness=-1)
-    cv.line(draw_image, (int((x1 + x2) / 2), int((y1 + y2) / 2)),
-            (x2 - int(rectangle_width / 10), y1 + int(rectangle_height / 10)),
-            color, int(rectangle_width / 80))
-    cv.line(draw_image,
-            (x2 - int(rectangle_width / 10), y1 + int(rectangle_height / 10)),
-            (x2 + int(rectangle_width / 2), y1 + int(rectangle_height / 10)),
-            color, int(rectangle_width / 80))
+        draw_image, drawpoint1, int(rectangle_width / 40), color, thickness=-1)
+    cv.line(draw_image, drawpoint1, drawpoint2, color,
+            int(rectangle_width / 80))
+    cv.line(draw_image, drawpoint2, drawpoint3, color,
+            int(rectangle_width / 80))
 
     if (font is not None) and (text is not None):
-        draw_image = CvDrawText.puttext(
-            draw_image, text,
-            (x2 - int(rectangle_width / 10),
-             y1 + int(rectangle_height / 10) - int(font_size * 1.5)), font,
-            font_size, color)
+        draw_image = CvDrawText.puttext(draw_image, text, textpoint, font,
+                                        font_size, color)
 
     return draw_image
